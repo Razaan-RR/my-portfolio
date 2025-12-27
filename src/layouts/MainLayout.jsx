6b8a8868bb5020403bf2d447ac79de18b539b6dc
Razaan <razaanreza0705@gmail.com>
2025-12-27 18:40:53 +0600

    main layout added

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,15 @@
+import Navbar from '../components/Navbar'
+import { Outlet } from 'react-router'
+import Footer from '../components/Footer'
+
+function MainLayout() {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <Outlet></Outlet>
+      <Footer></Footer>
+    </div>
+  )
+}
+
+export default MainLayout
