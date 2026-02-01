bc76e54d95998137652fe85b07f11bd9404b65af
Razaan <razaanreza0705@gmail.com>
2026-02-01 23:13:18 +0600

    updated mainlayout

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -7,12 +7,10 @@ function MainLayout() {
     <div className="min-h-screen flex flex-col bg-gradient-to-b from-slate-100 via-slate-200 to-slate-100">
       <Navbar />
 
-      {/* Page Content */}
       <main className="flex-1 pt-32">
         <Outlet />
       </main>
 
-      {/* Footer */}
       <Footer />
     </div>
   )
